e436a34f5bbdc1357b384ff08e1c9742a4da77da
Cjay Landero <xjaylandero@gmail.com>
2024-01-18 19:53:48 +0800

    Added Cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const activitiesRoutes = require("./routes/Activity");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
@@ -10,6 +11,7 @@ const connectDB = require("./config/db");
 // Connect DB
 connectDB();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
--- src/routes/Activity.js
+++ src/routes/Activity.js
@@ -15,15 +15,11 @@ router.get("/", async (req, res) => {
 
     const randInt = Math.floor(Math.random() * arrOfActivities.length);
 
-    res.send(arrOfActivities[randInt].activityTitle);
+    res.send(arrOfActivities[randInt]);
   } catch (err) {
     res.status(404);
     res.send(err);
   }
-});
-
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
 });
 
 module.exports = router;
